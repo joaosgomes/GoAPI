429ba5dbc5d6064ab24ec5e934a1ebbeae3e18f0
joaosgomes <joao.s.gomes@outlook.pt>
2023-07-19 05:22:51 +0100

    .

---
+++ README.md
@@ -0,0 +1,34 @@
+
+
+## <https://shell.cloud.google.com/>
+
+````
+
+
+
+go mod init example.com/GoAPI
+
+go get .
+
+
+
+
+go run go.go 
+
+OR
+
+go get .
+
+go run .
+
+
+
+
+echo "# GoAPI" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/joaosgomes/GoAPI.git
+git push -u origin main
+````
---
+++ main.go
@@ -0,0 +1,44 @@
+package Main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getGo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Gin API in Go")
+}
+
+func getRedirectGo(c *gin.Context) {
+	c.Redirect(http.StatusMovedPermanently, "http://go.dev/")
+}
+
+func getStatusGo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"Status": "Ok",
+	})
+}
+
+const Constant string = "Go Constant"
+
+func Main() {
+	fmt.Println("go build go.go")
+
+	var variable = "Go"
+	fmt.Println(variable)
+
+	fmt.Println(Constant)
+
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	router := gin.Default()
+	router.GET("/", getGo)
+	router.GET("/redirect", getRedirectGo)
+	router.GET("/status", getStatusGo)
+
+	router.Run("localhost:8080")
+}
